08782e564f6d40316c67a190e8103a071216a515
何梅 <hemei@zbj.com>
2017-07-26 22:37:39 +0800

    init project

---
+++ src/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container">test</div>
+</template>
+<script>
+export default {
+}
+</script>
+<style rel="stylesheet/less" lang="less">
+  .container {
+    width: 1190px;
+    margin: 0 auto;
+  }
+</style>
