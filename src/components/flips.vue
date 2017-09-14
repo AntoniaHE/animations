39fb054e7150126f521a65aace876f8b0ab258ab
何梅 <hemei@zbj.com>
2017-09-14 15:07:01 +0800

    update animation

---
+++ src/components/flips.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="flips animation infinite a-flips"></div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+.flips {
+  width: 100px;
+  height: 100px;
+  background-color: #eee;
+  border: 1px solid #ccc;
+  font-size: 18px;
+  line-height: 100px;
+  text-align: center;
+  box-sizing: border-box;
+}
+</style>
+
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,34 +1,61 @@
 <template>
-  <div class="container">
-    <div class="animation-item" v-for="animation in animationList" :key="animation">
-      <component :is="animation" />
-    </div>
+  <div class="container clearfix">
+     <div class="animation-item" v-for="item in animationList" :key="item"> 
+       <div class="animation infinite" :class="item"></div>
+     </div>
   </div>
 </template>
 
 <script>
-import AFadeIn from '@/components/a-fade-in'
-
 export default {
   data: function () {
     return {
       animationList: [
-        'aFadeIn'
+        'a-bounce',
+        'a-flash',
+        'a-enlarge',
+        'a-shrink',
+        'a-scale',
+        'a-shakex',
+        'a-shakey',
+        'a-swing',
+        'a-wave',
+        'a-fade-in',
+        // 'a-fade-in-left',
+        // 'a-fade-in-right',
+        // 'a-fade-in-top',
+        // 'a-fade-in-bottom',
+        'a-fade-out',
+        // 'a-fade-out-left',
+        // 'a-fade-out-right',
+        // 'a-fade-out-top',
+        // 'a-fade-out-bottom'
+        'a-light-speed-in'
       ]
     }
-  },
-  components: {
-    AFadeIn
   }
 }
 </script>
 
 <style rel="stylesheet/less" lang="less">
   .container {
-    width: 1190px;
+    width: 600px;
     margin: 0 auto;
   }
   .animation-item {
-    flex: 1;
+    width: 100px;
+    height: 100px;
+    margin: 10px;
+    float: left;
+    div {
+      width: 100px;
+      height: 100px;
+      background-color: #eee;
+      border: 1px solid #ccc;
+      font-size: 18px;
+      line-height: 100px;
+      text-align: center;
+      box-sizing: border-box;
+    }
   }
 </style>
